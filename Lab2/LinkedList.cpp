16be3eb6bebbb3106375bb5de111c297d81fb840
zbruennig <zkb@ku.edu>
2017-01-31 12:40:53 -0600

    Update LinkedList.cpp
    So Lab 2 is coming up for the 268 kiddos taking it in the Spring semester. Normally I don't go into this much work to protect my code, but this lab is especially important for them and I think they need to be able to create a Linked List on their own. Sorry friends, write your own code like the rest of us.

---
+++ Lab2/LinkedList.cpp
@@ -0,0 +1,89 @@
+/**
+*	@file LinkedList.cpp
+*	@author Zachary Bruennig 
+*	@date 9/19/16
+*	@brief A list data structure template which contains a pointer to a chain of nodes and methods
+*              to manipulate and access that chain.
+*/
+
+#include "LinkedList.h"
+#include "Node.cpp"
+#include <stdexcept>
+
+template <typename T>
+LinkedList<T>::LinkedList()
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+LinkedList<T>::~LinkedList()
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+bool LinkedList<T>::isEmpty() const
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+int LinkedList<T>::getLength() const
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+int LinkedList<T>::positionOf(T value) const
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+void LinkedList<T>::addBack(T value)
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+void LinkedList<T>::addFront(T value)
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+bool LinkedList<T>::insert(int position, T value)
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+bool LinkedList<T>::removeBack()
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+bool LinkedList<T>::removeFront()
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+bool LinkedList<T>::removeAt(int position)
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+void LinkedList<T>::setEntry(int position, T value)
+{
+/* Sorry, no help here! */
+}
+
+template <typename T>
+T LinkedList<T>::getEntry(int position) const
+{
+/* Sorry, no help here! */
+}
